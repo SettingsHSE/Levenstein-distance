1e1fbcb437a664a6dcac3eebb6aa46a04559b549
Софья Андреева <skandreeva@edu.hse.ru>
2020-04-21 15:01:12 +0300

    fix

---
+++ cmake-build-debug/CMakeFiles/LevnsteinDistance.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LevnsteinDistance.dir/main.cpp.obj"
+  "LevnsteinDistance.pdb"
+  "LevnsteinDistance.exe"
+  "LevnsteinDistance.exe.manifest"
+  "libLevnsteinDistance.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LevnsteinDistance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
